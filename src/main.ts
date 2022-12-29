18b5b470c3b16cb05c75cc0930a91df624209cbf
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2022-12-29 09:43:23 +0100

    :fire: project ready for now

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ const userPosition = {
 // variabel för sök radius
 let chosenDistance: number;
 const names: string[] = [];
+const markers: google.maps.Marker[] = [];
+let randomResturant: string = '';
 
 // Variablar för användar input
 const selectedDistance = document.querySelectorAll('input[name="distance"]') as NodeListOf<Element>;
@@ -37,19 +39,30 @@ let infowindow: any;
 
 // ======= FUNKTIONER ======// //======= FUNKTIONER ======// //======= FUNKTIONER ======// //======= FUNKTIONER ======//
 
+// tar värdet från valt avstånd
 function getSelectedDistance(e: any) {
   if (e.currentTarget.checked) {
-    console.log(`You selected ${e.currentTarget.value}`);
     chosenDistance = e.currentTarget.value;
   }
 }
 
+// skapar våra markers och inforutor
 function createMarker(place: google.maps.places.PlaceResult) {
   names.push(place.name);
+  let picture;
+
+  picture = place.photos;
+
+  if (picture !== undefined) {
+    picture = picture[0].getUrl({ 'maxWidth': 100, 'maxHeight': 100 });
+  }
+  else {
+    picture = 'no_resturant_picture.jpg';
+  }
 
   const contentString: string = `<h4>${place.name}</h4>`
-  + `Betyg: ${place.rating}<br>`
-  + `Öppet nu: byt till openNow <br>`;
+  + `<img height="100" src="${picture}" alt="${place.name}"> <br>`
+  + `Betyg: ${place.rating}<br>`;
 
   if (!place.geometry || !place.geometry.location) {
     return;
@@ -58,10 +71,12 @@ function createMarker(place: google.maps.places.PlaceResult) {
   const markerResult = new google.maps.Marker({
     map,
     position: place.geometry.location,
+    animation: google.maps.Animation.DROP,
   });
 
-  console.dir(place);
+  markers.push(markerResult);
 
+  // här skapas logiken för inforutorna
   google.maps.event.addListener(markerResult, 'click', () => {
     infowindow = new google.maps.InfoWindow({
       content: contentString,
@@ -72,13 +87,23 @@ function createMarker(place: google.maps.places.PlaceResult) {
       map,
     });
   });
+
+  // animation för vårt slumpade resultat
+  if (place.name === randomResturant) {
+    setTimeout(function() {markerResult.setAnimation(google.maps.Animation.BOUNCE)}, 1000);
+  }
 }
 
 // Skriv ut resultaten på kartan
 function handleResults(results: string | any[], status: any) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (let i = 0; i < results.length; i++) {
-      // printa en kartnål
+      // printa en kartnål för vårat slumpresultat
+      if (results.length === 1) {
+        createMarker(results[i]);
+        map.setCenter(results[0].geometry!.location!);
+      }
+      // printar alla kartnålar
       if (results[i].rating >= minRating.value) {
         createMarker(results[i]);
       }
@@ -90,6 +115,11 @@ function handleResults(results: string | any[], status: any) {
 // zoom: högre värde mer zoom
 // center: vart kartan ska fokusera
 function initMap(): void {
+  // nollställer variablar
+  resultInfo.textContent = '';
+  randomResturant = '';
+
+  // skapande av kartan
   map = new google.maps.Map(
     document.getElementById('map') as HTMLElement,
     {
@@ -103,7 +133,6 @@ function initMap(): void {
     position: userPosition,
     map: map,
     icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-    label: 'X'
   });
 
   // Leta närliggande restauranger inom radie utifrån användarens position
@@ -119,17 +148,43 @@ function initMap(): void {
 
   // Visar vår resultat ruta
   roulette.style.display = 'block';
+  mapDiv.style.display = 'block';
 
   // scrollar till rätt plats
   mapDiv.scrollIntoView();
 }
 
-function rouletteLunch() {
-  const randomResturant = names[Math.floor(Math.random() * names.length)];
-  resultInfo.textContent = `Du ska äta på ${randomResturant}`;
-  resultInfo.scrollIntoView();
+// Sets the map on all markers in the array.
+function setMapOnAll(map: google.maps.Map | null) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
 }
 
+// Removes the markers from the map, but keeps them in the array.
+function hideMarkers(): void {
+  setMapOnAll(null);
+}
+
+// slumpar fram vår resturang
+function rouletteLunch() {
+  const randomNumber = Math.floor(Math.random() * names.length)
+  randomResturant = names[randomNumber];
+  resultInfo.textContent = `Du ska äta på ${randomResturant}`;
+  resultInfo.scrollIntoView();
+  hideMarkers();
+
+  const request = {
+    query: randomResturant,
+    fields: ['name', 'rating', 'geometry', 'photos'],
+  };
+
+  // Gör en sökning… vänta på resultaten
+  service = new google.maps.places.PlacesService(map);
+  service.findPlaceFromQuery(request, handleResults);
+}
+
+// visar vår sliders värde
 function updateRating() {
   const numrating = minRating.value;
   const resultRating = Number(numrating).toFixed(1);
@@ -140,8 +195,6 @@ function updateRating() {
 function positionSuccess(position: any) {
   userPosition.lat = position.coords.latitude;
   userPosition.lng = position.coords.longitude;
-
-  console.log(userPosition.lat, userPosition.lng);
 }
 
 // Hittade ingen position
@@ -157,7 +210,6 @@ if (navigator.geolocation) {
 }
 
 // Eventlistner för val av distance
-
 for (let i = 0; i <= selectedDistance!.length; i++) {
   selectedDistance[i]?.addEventListener('change', getSelectedDistance);
 }
